9dcd0ac608641f8437b903300b3a5d1eb0d9343e
Andrew Bell <fiixed@gmail.com>
2014-09-07 09:08:45 -0500

    Initial Commit

---
+++ Out of this World/FIXViewController.m
@@ -0,0 +1,29 @@
+//
+//  FIXViewController.m
+//  Out of this World
+//
+//  Created by Andrew Bell on 9/7/14.
+//  Copyright (c) 2014 Andrew Bell. All rights reserved.
+//
+
+#import "FIXViewController.h"
+
+@interface FIXViewController ()
+
+@end
+
+@implementation FIXViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
